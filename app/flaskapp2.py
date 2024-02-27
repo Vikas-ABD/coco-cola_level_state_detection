3abd3a6bb25f964dba2d6622071862beb11867af
vikas_chelluru <112474932+Vikas-ABD@users.noreply.github.com>
2024-02-27 22:29:04 +0530

    first

--- app/flaskapp2.py
+++ app/flaskapp2.py
@@ -100,5 +100,5 @@ def detect():
     return render_template('index.html', result_image='/static/result.jpg')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False,post="0.0.0.0")
 
